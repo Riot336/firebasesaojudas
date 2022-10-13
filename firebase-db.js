0dd9e18cb43358cd6c4a2e75dc5cdd7657b77f53
Daniel N <neris1003@hotmail.com>
2022-10-13 11:43:23 -0300

    Aula de hoje

--- firebase-db.js
+++ firebase-db.js
@@ -5,7 +5,13 @@ import { getDatabase } from "firebase/database";
 // incluir as configurações obtidas no firebase console 
 //
 const firebaseConfig = {
-  
+        apiKey: "AIzaSyDpctpkyTVjbB5JDisnb8ceritnrFpQCKs",
+        authDomain: "usabilidade1310.firebaseapp.com",
+        databaseURL: "https://usabilidade1310-default-rtdb.firebaseio.com",
+        projectId: "usabilidade1310",
+        storageBucket: "usabilidade1310.appspot.com",
+        messagingSenderId: "627767609061",
+        appId: "1:627767609061:web:9a93a4bfc2739c83cbf60c"
 };
 
 // Initialize Firebase
